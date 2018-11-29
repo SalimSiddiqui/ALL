9596d64253e42c8960508d298c5ab4e9c2c5ffd1
Salim Siddiqui <salimcyber786@gmail.com>
2018-11-29 15:59:49 +0530

    Added angular project

---
+++ AngularJS/Ng/EmployeePortal/controller.js
@@ -0,0 +1,70 @@
+﻿angular.module('crudFactoryApp.controller', []).
+
+   /* Company controller */
+  controller('companyController', function ($scope, crudAPIFactory) {
+      $scope.createCompany = function (Company) {
+          //Insert new Company.
+          crudAPIFactory.createCompany(Company).success(function (response) {
+              crudAPIFactory.getCompanyList().success(function (response) {
+                  $scope.Companies = response;
+                  $scope.clearUI();
+              });
+
+          });
+      };
+
+      $scope.getCompany = function (Company) {
+          //Get Company.
+          crudAPIFactory.getCompany(Company).success(function (response) {
+              // one Way to define object 
+              $scope.Company = {};            
+              $scope.Company.name = response[0].name
+             
+              // Other way to defind Object 
+              $scope.Company = {
+                  companyId: response[0].companyId,
+                  name: response[0].name,
+                  ceo: response[0].ceo,
+                  country: response[0].country,
+                  foundationYear: response[0].foundationYear,
+                  noOfEmployee: response[0].noOfEmployee
+              };             
+          });
+      };
+
+      $scope.updateCompany = function (Company) {
+          //Update Company.
+          crudAPIFactory.updateCompany(Company).success(function (response) {
+              crudAPIFactory.getCompanyList().success(function (response) {
+                  $scope.Companies = response;
+              });
+              $scope.clearUI();
+          });
+      };
+
+      $scope.deletecompany = function (Company) {
+          //Update Company.
+          crudAPIFactory.deletecompany(Company).success(function (response) {
+              crudAPIFactory.getCompanyList().success(function (response) {
+                  $scope.Companies = response;
+              });
+          });
+      };
+
+      //Get Company List
+      crudAPIFactory.getCompanyList().success(function (response) {
+          //$scope.Companies = response.data;
+         
+          $scope.Companies = response;
+      });
+
+      $scope.clearUI = function () {
+          $scope.Company.companyId = "";
+          $scope.Company.name = "";
+          $scope.Company.ceo = "";
+          $scope.Company.country = "";
+          $scope.Company.foundationYear = "";
+          $scope.Company.noOfEmployee = "";
+      };
+
+  });
---
+++ AngularJS/Ng/EmployeePortal/crudFactory.js
@@ -0,0 +1,63 @@
+﻿angular.module('crudFactoryApp.crudFactory', [])
+  .factory('crudAPIFactory', function ($http) {
+
+      var crudFactory = {};
+
+      //Get Company List
+      crudFactory.getCompanyList = function () {
+          return $http({
+              // url: "http://localhost:8080/SpringMavenRestDemoService/getcompanyall/",
+              url: "../WebService/DDL_Service.asmx/GetCompData",
+              method: 'GET'
+          });
+      };
+
+      //Insert new Company.
+      crudFactory.createCompany = function (Company) {
+         
+          var obj = {};
+          obj.Company = Company;
+          console.log(obj.Company);
+          return $http({
+              url: '../WebService/DDL_Service.asmx/InsertCompData',           
+              method: 'POST',
+              //headers: {
+              //    "Content-Type": "application/json"
+              //},
+              data: obj
+          });
+         
+         
+      };
+
+      //Get Company.
+      crudFactory.getCompany = function (Company) {
+         
+          return $http({
+            //  url: "http://localhost:8080/SpringMavenRestDemoService/getcompany/" + Company.companyId,
+              url: '../WebService/DDL_Service.asmx/CompData?companyId=' + Company.companyId,
+              method: 'GET',
+          });
+      };
+
+      //Update Company.
+      crudFactory.updateCompany = function (Company) {
+          var obj = {};
+          obj.Company = Company;
+          return $http({
+              url: '../WebService/DDL_Service.asmx/CompDataUpdate',
+              method: 'POST',
+              data: obj,
+          });
+      };
+
+      //Delete Company.
+      crudFactory.deletecompany = function (Company) {
+          return $http({
+              url: '../WebService/DDL_Service.asmx/CompDatadelete?companyId=' + Company.companyId,
+              method: 'GET',
+          });
+      };
+
+      return crudFactory;
+  });
