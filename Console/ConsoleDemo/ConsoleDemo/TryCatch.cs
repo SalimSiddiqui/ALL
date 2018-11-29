6cdc011f93ff7d3c3889071b3272fe93b65cd4be
Salim Siddiqui <salimcyber786@gmail.com>
2018-11-29 16:07:42 +0530

    ConsoleDemo project

---
+++ Console/ConsoleDemo/ConsoleDemo/TryCatch.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleDemo
+{
+   public class TryCatch
+    {
+        public void print()
+        {
+            try
+            {
+
+                try
+                {
+
+                }
+                catch (Exception ex)
+                {
+
+                    
+                }
+            }
+            catch (DivideByZeroException e)
+            {
+
+            }
+            catch (Exception ex)
+            {
+                              
+            }
+            
+        }
+
+
+        public void printnew()
+        {
+            try
+            {
+                int b = 0;
+                int a =  1/b;
+            }
+            finally
+            {
+            }
+        }
+        // Eroor --Severity	Code	Description	Project	File	Line	Suppression State
+
+            //Expected catch or finally
+        //public void printnew1()
+        //{
+        //    try
+        //    {
+        //        int b = 0;
+        //        int a = 1 / b;
+        //    }
+        //}
+    }
+}
